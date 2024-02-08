364d55ded5247cf38b86cad66d9f38053dbca8dc
cty20010831 <congtianyue233@gmail.com>
2024-02-08 01:23:52 -0600

    Changed get_pub_info

--- scraping_helper_functions/get_pub_info.py
+++ scraping_helper_functions/get_pub_info.py
@@ -1,20 +1,123 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
 import time
 import pandas as pd
-from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, StaleElementReferenceException
-import chardet
+from .webdriver_setup import initialize_driver
+import os
+
+def get_pub_url(awarded_year):
+    '''
+    Creates a csv of awarded authors' publication-related basic information 
+    (title, number of citation, and, most importantly, paper url on the 
+    Google Scholar) gained from going into author's Google Scholar Page.
+    This paper url will be further "clicked" by selenium to extract the abstract.
+
+    Inputs: 
+        1) awarded_year: year at which the author is awarded NSF
+
+    Returns: path for pub_url
+    '''
+
+    # Define the path storing author info (i.e., `author_info` table)
+    author_info_path = f"database/author_info_{awarded_year}.csv"
+
+    # Define the path for the output (primarily, publication url)
+    pub_url_path = f"database/pub_url_{awarded_year - 3}_{awarded_year + 3}.csv"
+
+    # Make sure the author_info table exists
+    try:
+        df = pd.read_csv(author_info_path)
+    except FileNotFoundError:
+        print(f"Failed to run the function because {author_info_path} is not found.\n")
+        print("Please get author_info first.")
+
+        # Leave the function
+        return
+
+    # Create an empty DataFrame to store all authors' publication information
+    all_publications = pd.DataFrame()
+
+    for _, row in df.iterrows():
+        # Configure webdriver (again) every time after quitted for each iteration
+        driver = initialize_driver()
+
+        url = row['url']
+
+        # Visit the author's homepage
+        driver.get(url)
+        time.sleep(2)
+
+        # Click the "Show more" button until all papers are loaded
+        while True:
+            try:
+                show_more_button = driver.find_element(By.ID, "gsc_bpf_more")
+                if show_more_button.is_displayed() and show_more_button.is_enabled():
+                    show_more_button.click()
+                    time.sleep(2)
+                else:
+                    break
+            except (NoSuchElementException, ElementClickInterceptedException):
+                break
+
+        # Extract information about the papers
+        publications = []
+        rows = driver.find_elements(By.CSS_SELECTOR, "tr.gsc_a_tr")
+        for row in rows:
+            # Focus on 3 years before and after the awarded_year
+            publication_year = row.find_element(By.CSS_SELECTOR, "span.gsc_a_hc").text
+            if publication_year:
+                publication_year = int(publication_year)
+                # Check if publication_year falls within the desired range
+                if awarded_year - 3 <= publication_year <= awarded_year + 3:
+                    title = row.find_element(By.CSS_SELECTOR, "a.gsc_a_at").text
+                    cited_by = row.find_element(By.CSS_SELECTOR, "a.gsc_a_ac").text
+                    paper_url = row.find_element(By.CSS_SELECTOR, "a.gsc_a_at").get_attribute("href")
+
+                    publications.append({
+                        "Title": title,
+                        "Year": publication_year,
+                        "Cited by": cited_by,
+                        "Paper URL": paper_url
+                    })
+
+        # Convert the author's publication information into a DataFrame
+        author_publications_df = pd.DataFrame(publications)
+
+        # Add the author's publication information to the overall DataFrame
+        all_publications = pd.concat([all_publications, author_publications_df], ignore_index=True)
+        all_publications.to_csv(pub_url_path, index=False, encoding='utf-8-sig')
+        driver.quit()
+
+    # Save all authors' publication information as a CSV file
+    all_publications.to_csv(pub_url_path, index=False, encoding='utf-8-sig')
+
+    return pub_url_path
+
 
 def extract_info_from_html(publication_url, driver):
+    '''
+    Builds on `get_pub_url` function to extract publication-related information.
+
+    Note: The reaon for separating this scraping publication information 
+    process into two steps is to prevent anti-scraping.
+
+    Inputs: 
+        1) publication_url: url that can lead to paper detail with an additional click of Google Scholar
+        2) driver: selenium driver
+
+    Returns: a dictionary containing authors' names, publication date, 
+        journal name, paper abstract, and yearly breakdown of paper citation 
+    '''
+
     driver.get(publication_url)
-    time.sleep(3)  # Wait for the page to load
+    # Wait for the page to load
+    time.sleep(3)  
 
     # Initialize variables to store extracted information
     authors = ""
     publication_date = ""
     journal = ""
     abstract = ""
-    citation_list = []
     year_citations = {}
 
     # Extract authors
@@ -50,7 +153,7 @@ def extract_info_from_html(publication_url, driver):
         except:
             abstract = "Abstract not found"
 
-    # Extract citations and years
+    # Extract citations and years of the paper
     try:
         year_elements = driver.find_elements(By.CSS_SELECTOR, "div#gsc_oci_graph_bars span.gsc_oci_g_t")
         citation_elements = driver.find_elements(By.CSS_SELECTOR, "div#gsc_oci_graph_bars a.gsc_oci_g_a span.gsc_oci_g_al")
@@ -68,60 +171,81 @@ def extract_info_from_html(publication_url, driver):
         "Citations": year_citations
     }
 
-# Configure Selenium WebDriver
-options = webdriver.ChromeOptions()
-# Set browser options
-options.add_argument('--disable-blink-features=AutomationControlled')
-options.add_argument('--headless=new')
-# Here, ensure that chromedriver.exe is in system PATH
-# Check by `where chromedriver`` (for Windows) or `which chromedriver` (for MAC)
-# If not, move it to the system PATH to avoid running webdriver.ChromeService()
-driver = webdriver.Chrome(options=options)
 
-# Read CSV file
-input_csv_file = "author_publications_2016_2022.csv"
-output_csv_file = "pub_info.csv"
+def generate_pub_info_table(awarded_year):
+    '''
+    Generates the final csv table storing publication-related information after 
+    relating it to NSF award table and author_info table.
 
-df = pd.read_csv(input_csv_file)
+    Note: The reaon for separating this scraping publication information 
+    process into two steps is to prevent anti-scraping.
 
-# Create new columns to store extracted information
-df["Authors"] = ""
-df["Publication Date"] = ""
-df["Journal"] = ""
-df["Abstract"] = ""
-df["Citations"] = ""
+    Inputs: 
+        1) awarded_year: year at which the author is awarded NSF
+        2) pub_url_path: path storing the temporarily stored csv containing pub_url
 
-# Iterate through each row, execute the scraping function, and update the DataFrame
-for index, row in df.iterrows():
+    Returns: None
+    '''
 
-    publication_url = row["Paper URL"]
+    # Configure Selenium WebDriver
+    driver = initialize_driver()
+
+    # Run the function to generate pub_url table and get the pub_url_path
+    pub_url_path = get_pub_url(awarded_year)    
+
+    # Defines the path for the final pub_info table
+    pub_info_path = pub_url_path.replace("pub_url", "pub_info")
+
+    # Build the output pub_info table based on the previous pub_url dataframe
     try:
-        info = extract_info_from_html(publication_url, driver)
-        print(info)
-        df.at[index, "Authors"] = info["Authors"]
-        df.at[index, "Publication Date"] = info["Publication Date"]
-        df.at[index, "Journal"] = info["Journal"]
-        df.at[index, "Abstract"] = info["Abstract"]
-        df.at[index, "Citations"] = str(info["Citations"])
-    except:
-        pass
+        # Make sure the pub_url_path table exists
+        df = pd.read_csv(pub_url_path)
+    except FileNotFoundError:
+        print(f"Failed to run the function because {pub_url_path} is not found.\n")
+        print("Please get pub_url first.")
+        # Leave the function
+        return
 
-    time.sleep(2)
-    # Save data every 50 rows
-    if (index + 1) % 50 == 0:
-        df.to_csv(output_csv_file, index=False, encoding='utf-8-sig')
-        print(f"Saved data for {index + 1} rows.")
+    # Create new columns to store extracted information
+    df["Authors"] = ""
+    df["Publication Date"] = ""
+    df["Journal"] = ""
+    df["Abstract"] = ""
+    df["Citations"] = ""
 
-    # Close and restart WebDriver
-    if (index + 1) % 50 == 0:
-        driver.quit()
-        print("Driver closed. Sleeping for 20 seconds...")
-        time.sleep(20)
-        # Restart WebDriver
-        driver = webdriver.Chrome(options=options)
+    # Iterate through each row, execute the scraping function, and update the DataFrame
+    for index, row in df.iterrows():
+        publication_url = row["Paper URL"]
+        try:
+            info = extract_info_from_html(publication_url, driver)
+            print(info)
+            df.at[index, "Authors"] = info["Authors"]
+            df.at[index, "Publication Date"] = info["Publication Date"]
+            df.at[index, "Journal"] = info["Journal"]
+            df.at[index, "Abstract"] = info["Abstract"]
+            df.at[index, "Citations"] = str(info["Citations"])
+        except:
+            pass
 
-# Save remaining data
-df.to_csv(output_csv_file, index=False, encoding='utf-8-sig')
+        time.sleep(2)
+        # Save data every 50 rows
+        if (index + 1) % 50 == 0:
+            df.to_csv(pub_info_path, index=False, encoding='utf-8-sig')
+            print(f"Saved data for {index + 1} rows.")
 
-# Close WebDriver
-driver.quit()
+        # Close and restart WebDriver
+        if (index + 1) % 50 == 0:
+            driver.quit()
+            print("Driver closed. Sleeping for 20 seconds...")
+            time.sleep(20)
+            # Restart WebDriver
+            driver = initialize_driver()
+
+    # Save remaining data
+    df.to_csv(pub_info_path, index=False, encoding='utf-8-sig')
+
+    # Close WebDriver
+    driver.quit()
+
+    # Remove the intermediate pub_url file after finish running this function
+
